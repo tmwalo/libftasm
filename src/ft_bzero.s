e6821be3c1161877a8e0312a96321a4041c54153
Terence MWALO <tmwalo@e4r13p6.wethinkcode.co.za>
2018-12-06 12:37:53 +0200

    fix: function name

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -6,16 +6,17 @@
 #    By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/12/05 14:49:17 by tmwalo            #+#    #+#              #
-#    Updated: 2018/12/05 15:27:53 by tmwalo           ###   ########.fr        #
+#    Updated: 2018/12/06 10:56:43 by tmwalo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 segment		.text
-global		ft_bzero
-extern		ft_memset
+global		_ft_bzero
+extern		_ft_memset
 
-ft_bzero:
+_ft_bzero:
 	mov		rdx, rsi			; place size = 2nd arg of ft_bzero as 3rd arg of ft_memset
 	xor		rsi, rsi			; 2nd arg of ft_memset = 0
-	call	ft_memset
+	call	_ft_memset
+	xor		rax, rax
 	ret

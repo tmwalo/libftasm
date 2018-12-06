d5a8e8fb7bd0388857e67b9db757475ba8d255c8
Terence MWALO <tmwalo@e4r13p6.wethinkcode.co.za>
2018-12-06 12:36:21 +0200

    add print memory bytes

--- src/main.c
+++ src/main.c
@@ -1,11 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/06 11:26:35 by tmwalo            #+#    #+#             */
+/*   Updated: 2018/12/06 12:29:53 by tmwalo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libfts.h"
 #include <stdio.h>
+
+void	print_mem_bytes(char *str, int num_bytes) {
+	for (int i = 0; i < num_bytes; ++i) {
+		if (str[i] != '\0')
+			printf("%c", str[i]);
+		else
+			printf("%c", '0');
+	}
+}
 
 int		main(void) {
 
 	char	*str;
 	char	*memset_str;
+	char	*libc_bzero_str;
 	char	*bzero_str;
 
 	str = "battle not with monsters, lest ye become one";
@@ -15,21 +36,29 @@ int		main(void) {
 
 	printf("ft_memset:\n");
 	memset_str = strdup(str);
-	printf("memset_str: %s\n\n", memset_str);
-	memset(memset_str, 'x', 10);
-	printf("set 4 bytes to 'x': %s\n\n", memset_str);
+	printf("set 12 bytes to 'x', libc: %s\n", memset(strdup(str), 'x', 12));
+	ft_memset(memset_str, 'x', 12);
+	printf("set 12 bytes to 'x', nasm: %s\n\n", memset_str);
 
 	/* ft_bzero */
 
 	printf("ft_bzero:\n");
+	libc_bzero_str = strdup(str);
 	bzero_str = strdup(str);
+	bzero(libc_bzero_str, 4);
+	printf("set 4 bytes to zero, libc: ");
+   	print_mem_bytes(libc_bzero_str, (int) strlen(str));
+	printf("\n");
 	ft_bzero(bzero_str, 4);
-	printf("set 4 bytes to zero: %s\n\n", bzero_str);
+	printf("set 4 bytes to zero, nasm: ");
+   	print_mem_bytes(bzero_str, (int) strlen(str));
+	printf("\n\n");
 
 	/* cleanup */
 
 	free(memset_str);
 	free(bzero_str);
+	free(libc_bzero_str);
 
 	return (0);
 

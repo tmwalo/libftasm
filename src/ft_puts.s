5b3b4cb1de3efa41d7e0efee6bac4761271c2e9e
Terence MWALO <tmwalo@e4r13p6.wethinkcode.co.za>
2018-12-12 08:53:10 +0200

    fix: add underscore to function name and add function prototype to header

--- src/ft_puts.s
+++ src/ft_puts.s
@@ -6,14 +6,14 @@
 #    By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/12/08 14:58:21 by tmwalo            #+#    #+#              #
-#    Updated: 2018/12/08 14:59:12 by tmwalo           ###   ########.fr        #
+#    Updated: 2018/12/12 08:49:57 by tmwalo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 segment		.text
-global		ft_puts
+global		_ft_puts
 
-ft_puts:
+_ft_puts:
 	push	rbp
 	mov		rbp, rsp
 	sub		rsp, 48								; allocate stack memory
--- src/libfts.h
+++ src/libfts.h
@@ -6,7 +6,7 @@
 /*   By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 11:24:36 by tmwalo            #+#    #+#             */
-/*   Updated: 2018/12/06 11:24:55 by tmwalo           ###   ########.fr       */
+/*   Updated: 2018/12/12 08:50:53 by tmwalo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ char			**ft_strsplit(char const *s, char c);
 char			*ft_itoa(int n);
 void			ft_putchar(char c);
 void			ft_putstr(char const *s);
+void			ft_puts(char const *s);
 void			ft_putendl(char const *s);
 void			ft_putnbr(int n);
 void			ft_putchar_fd(char c, int fd);

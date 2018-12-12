53c8402d6e413836bbd83200a10674c7eda4646f
Terence MWALO <tmwalo@e4r13p6.wethinkcode.co.za>
2018-12-12 14:38:05 +0200

    fix: ft_strlen must use repeat str instructions

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,4 +1,14 @@
-
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_strlen.s                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/12/12 13:55:12 by tmwalo            #+#    #+#              #
+#    Updated: 2018/12/12 14:36:40 by tmwalo           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 
 segment		.text
 global		_ft_strlen
@@ -6,13 +16,11 @@ global		_ft_strlen
 _ft_strlen:
 	push	rbp
 	mov		rbp, rsp
-	xor		rcx, rcx
-while:
-	cmp		byte [rdi + 1 * rcx], 0
-	je		end
-	inc		rcx
-	jmp		while
-end:
-	mov		rax, rcx
+	cld							; clear direction flag
+	xor		rax, rax			; scan for null char
+	mov		rcx, -1				; two's complement has same bit pattern as max 64-bit value(since actual str len unknown before hand)
+	repne	scasb
+	mov		rax, -2
+	sub		rax, rcx
 	leave
 	ret
